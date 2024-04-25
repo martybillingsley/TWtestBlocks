d7869726ada5232629b7086fe7be7d4024f02c51
martybillingsley <48107311+martybillingsley@users.noreply.github.com>
2024-04-25 12:13:29 -0700

    added pins drawer/toolbox

---
+++ pins.ts
@@ -0,0 +1,30 @@
+/**
+ * Pins drawer
+ */
+//% color="#34ab3b" icon="\uf140" weight=100
+namespace Pins {
+
+
+    /**
+     * Read analog pin
+     * @param pin number
+     */
+    //% block="Analog Read  Pin %p"
+    //% blockID="Pins_analog_read"
+    export function analog_read(p: number): number {
+        return 0;
+    }
+
+
+    /**
+     * Read digital pin
+     * @param pin number
+     */
+    //% block="Digital Read  Pin %p"
+    //% blockID="Pins_digital_read"
+    export function digital_read(p: number): number {
+        return 0;
+    }
+
+}
+
--- remote.ts
+++ remote.ts
@@ -16,6 +16,7 @@ namespace Remote {
     }
 
 
+
     /**
      * Read remote
      * @param pin number
